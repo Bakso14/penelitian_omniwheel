dec03dc28ed5a290e99af29b73460d42252ab99e
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2024-07-13 09:42:49 +0700

    Update main.cpp

--- Omniwheel/src/main.cpp
+++ Omniwheel/src/main.cpp
@@ -1,49 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
-/*
- ***************************************************************************
 
-    Euler_Streaming.pde - part of sample SW for using BNO055 with Arduino
-
-   (C) All rights reserved by ROBERT BOSCH GMBH
-
-   Copyright (C) 2014 Bosch Sensortec GmbH
-
- 	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- **************************************************************************/
-/*	Date: 2014/01/07
- 	 Revision: 1.2
-
-*/
-
-#include "BNO055_support.h"		//Contains the bridge code between the API and Arduino
+#include "BNO055_support.h"		
 #include <Wire.h>
 
-//The device address is set to BNO055_I2C_ADDR2 in this example. You can change this in the BNO055.h file in the code segment shown below.
-// /* bno055 I2C Address */
-// #define BNO055_I2C_ADDR1                0x28
-// #define BNO055_I2C_ADDR2                0x29
-// #define BNO055_I2C_ADDR                 BNO055_I2C_ADDR2
-
-//Pin assignments as tested on the Arduino Due.
-//Vdd,Vddio : 3.3V
-//GND : GND
-//SDA/SCL : SDA/SCL
-//PSO/PS1 : GND/GND (I2C mode)
-
-//This structure contains the details of the BNO055 device that is connected. (Updated after initialization)
 struct bno055_t myBNO;
 struct bno055_euler myEulerData; //Structure to hold the Euler data
 
